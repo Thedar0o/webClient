791f72458c7870dd4c6b20288f8161ce7c8fcb51
Darek <darek.rolicz95@gmail.com>
2020-05-17 14:32:20 +0200

    Added Login and register, edited home page

---
+++ WebApp/WebApp/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult VerifyAccount(Account account)
+        {
+            using(ContactDBEntities db = new ContactDBEntities())
+            {
+                var userData = db.Account.FirstOrDefault(x => x.Login == account.Login && x.Password == account.Password);
+                if(userData == null)
+                {
+                    account.LoginErrorMessage = "Wrong username/password";
+                    return View("Login", account);
+                }
+                else
+                {
+                    Session["Id"] = userData.Id;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+           // return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddUser(int Id = 0)
+        {
+            Account account = new Account();
+            return View(account);
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(Account account)
+        {
+            using(ContactDBEntities db = new ContactDBEntities())
+            {
+                if(db.Account.Any(x => x.Login == account.Login))
+                {
+                    ViewBag.DuplicateMessage = "Username already exist";
+                    return View("AddUser");
+                }
+                db.Account.Add(account);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Reqistration Successful";
+            return View("AddUser", new Account());
+        }
+    }
+}
---
+++ WebApp/WebApp/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ContactController : Controller
+    {
+       ContactDBEntities db = new ContactDBEntities();
+       
+        // GET: Contact
+        public ActionResult Contacts()
+        {
+            var contact = db.Contacts.ToList();
+            return View("ContactsList", contact);
+        }
+
+        public JsonResult GetContacts()
+        {
+            var cont = db.Contacts.ToArray();
+            List<Contacts> listcont = new List<Contacts>();
+            foreach (var item in cont)
+            {
+                listcont.Add(new Contacts
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    SecoundName = item.SecoundName,
+                    Email = item.Email,
+                    Password = item.Password,
+                    CategoriesId = item.CategoriesId,
+                    PhoneNumber = item.PhoneNumber,
+                    BirthDate = item.BirthDate,
+
+                });
+            }
+            return Json(listcont, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
